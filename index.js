65f52b163ae7b46946ec108745ad6224cb7df56e
klaygomes <klaygomes@gmail.com>
2017-02-03 01:52:48 -0200

    code refactory

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const ts = require("gulp-typescript")
     , sourcemaps = require("gulp-sourcemaps")
     , Writable = require("stream").Writable
     , path = require("path")
+    , isObject = require("lodash.isobject")
+    , isDate = require("lodash.isdate")
+    , isArray = require("lodash.isarray")
+    , isFunction = require("lodash.isfunction")
+    , assignIn = require("lodash.assignin")
+    , reduce = require("lodash.reduce")
     , sep = path.sep;
 
 const state = {
@@ -13,52 +19,50 @@ const state = {
 let _currentState = state.idle;
 
 function factoryTypeScriptPreprocessor(logger, config, basePath) {
-
+    "use strict";
     /*
-     tsConfigPath must aways be present
+        tsConfigPath must aways be present
      */
+
     if (toString.call(config.tsconfigPath) !== "[object String]") {
         throw new Error("tsconfigPath was not defined");
     }
 
-
     /*
-     compilerOptions
+        compilerOptions
      */
     let compilerOptions = (config.compilerOptions || config.tsconfigOverrides) || {};
 
-    if (!_.isObject(compilerOptions) || _.isDate(compilerOptions)) {
+    if (!isObject(compilerOptions) || isDate(compilerOptions)) {
         throw new Error("compilerOptions if defined, must be an object.");
     }
 
     let defaultCompilerOptions = {
         outDir: undefined,
-        typescript: typescript
+        typescript: undefined
     };
 
-    _.extend(compilerOptions, defaultCompilerOptions);
+    assignIn(compilerOptions, defaultCompilerOptions);
 
 
     /*
      It is used to change virtual path of served files
      */
-    config.transformPath = config.transformPath || [function (filepath) {
-            return filepath.replace(/\.ts$/i, ".js");
-        }];
+    config.transformPath = config.transformPath || [(filepath)=> filepath.replace(/\.ts$/i, ".js")];
 
 
-    if (_.isFunction(config.transformPath)) {
+    if (isFunction(config.transformPath)) {
         config.transformPath = [config.transformPath];
-    } else if (!_.isArray(config.transformPath)) {
+    } else if (isArray(config.transformPath)) {
         throw new Error("transformPath must be an array or a function");
     }
 
     /*
      It is used to ignore files
      */
-    config.ignorePath = config.ignorePath || new Function;
+    config.ignorePath = config.ignorePath || function(){/*empty*/};
 
-    if (!_.isFunction(config.ignorePath)) {
+    if (!isFunction(config.ignorePath)) {
         throw new Error("ignorePath must be a function");
     }
 
@@ -68,62 +72,12 @@ function factoryTypeScriptPreprocessor(logger, config, basePath) {
         , tsconfigPath = path.resolve(basePath, config.tsconfigPath)
         , tsProject = ts.createProject(tsconfigPath, compilerOptions);
 
-    function compile() {
-        log.debug("Compiling ts files...");
-
-        _currentState = state.compiling;
-        _compiledBuffer = [];
-
-        let output = Writable({objectMode: true}),
-            tsResult = tsProject.src()
-                .pipe(sourcemaps.init())
-                .pipe(ts(tsProject));
-
-        // save compiled files in memory
-        output._write = function (chunk, enc, next) {
-            _compiledBuffer.unshift(chunk);
-            next();
-        };
-
-        tsResult.js
-            .pipe(sourcemaps.write(config.sourcemapOptions || {}))
-            .pipe(output);
-
-        //called at the end of compilation process
-        tsResult.js.on("end", function () {
-            log.debug("Compilation completed!");
-            _currentState = state.compilationCompleted;
-            _releaseBuffer();
-        });
-    }
-
-    function dummyFile(message) {
-        return "/* preprocessor:typescript --> " + message + " */";
-    }
 
     function transformPath(filepath) {
-        return _.reduce(config.transformPath, function (memo, clb) {
+        return reduce(config.transformPath, function (memo, clb) {
             //I simple ignore clb that was not function
-            return _.isFunction(clb) ? clb.call(config, memo) : memo;
+            return isFunction(clb) ? clb.call(config, memo) : memo;
         }, filepath);
-    }
-
-    //responsible to flush the cache and notify karma
-    function _releaseBuffer() {
-        let buffered;
-
-        while (buffered = _servedBuffer.shift()) {
-            _serveFile(buffered.file, buffered.done);
-            //it is possible to start compiling while releasing files
-            if (state.compilationCompleted != _currentState){
-                break;
-            }
-        }
-    }
-
-    //Used in idle or compiling states
-    function _feedBuffer(file, done) {
-        _servedBuffer.unshift({file: file, done: done});
     }
 
     //Used to normalize file paths
@@ -131,11 +85,20 @@ function factoryTypeScriptPreprocessor(logger, config, basePath) {
         return transformPath(path.replace(/[\/\\]/g, sep));
     }
 
+    //Used in idle or compiling states
+    function _feedBuffer(file, done) {
+        _servedBuffer.unshift({file: file, done: done});
+    }
+
+    function dummyFile(message) {
+        return `/* preprocessor:typescript --> ${message} */`;
+    }
+
     //Used to fetch files from buffer
     // if requested file contains a sha defined,
     //it means this file was changed by karma
     function _serveFile(requestedFile, done) {
-        var compiled
+        let compiled
             , temp = []
             , wasCompiled;
 
@@ -164,15 +127,52 @@ function factoryTypeScriptPreprocessor(logger, config, basePath) {
         //if file was not found in the stream
         //maybe it is not compiled or it is a definition file, so we don't need to worry about
         if (!wasCompiled) {
-            log.debug(requestedFile.originalPath + ' was not found. Maybe it was not compiled or it is a definition file.');
-            done(null, dummyFile('This file was not compiled'));
+            log.debug(`${requestedFile.originalPath} was not found. Maybe it was not compiled or it is a definition file.`);
+            done(null, dummyFile("This file was not compiled"));
         }
+    }
+
+    function compile() {
+        log.debug("Compiling ts files...");
+
+        _currentState = state.compiling;
+        _compiledBuffer = [];
+
+        let output = Writable({objectMode: true}),
+            tsResult = tsProject.src()
+                .pipe(sourcemaps.init())
+                .pipe(ts(tsProject));
+
+        // save compiled files in memory
+        output._write = function (chunk, enc, next) {
+            _compiledBuffer.unshift(chunk);
+            next();
+        };
+
+        tsResult.js
+            .pipe(sourcemaps.write(config.sourcemapOptions || {}))
+            .pipe(output);
+
+        //called at the end of compilation process
+        tsResult.js.on("end", function () {
+            //responsible to flush the cache and notify karma
+            log.debug("Compilation completed!");
+            _currentState = state.compilationCompleted;
+            let buffered;
+            while (buffered = _servedBuffer.shift()) {
+                _serveFile(buffered.file, buffered.done);
+                //it is possible to start compiling while releasing files
+                if (state.compilationCompleted !== _currentState){
+                    break;
+                }
+            }
+        });
     }
 
     //first compilation
     compile();
 
-    return function createTypeScriptPreprocessor(/*ignored*/content, file, done) {
+    return function createTypeScriptPreprocessor(/*ignored*/_, file, done) {
 
         //ignoring files
         if (!!config.ignorePath(file.path)) {
@@ -201,6 +201,10 @@ factoryTypeScriptPreprocessor.$inject = ["logger", "config.typescriptPreprocesso
 module.exports = {
     "preprocessor:typescript": ["factory", factoryTypeScriptPreprocessor]
 };
+
+
+
+
 
 
 
